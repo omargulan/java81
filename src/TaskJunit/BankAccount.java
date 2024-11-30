fe5f1cc9956e20f817c2f99235e7a9489a12356b
Sanzhar <runtimepcstudent@gmail.com>
2024-11-30 16:45:37 +0500

    30 november 2024

--- src/TaskJunit/BankAccount.java
+++ src/TaskJunit/BankAccount.java
@@ -12,6 +12,51 @@ public class BankAccount {
         this.firstName = firstName;
         this.secondName = secondName;
     }
+    // TODO: Снятие денег
+    public void withdraw(int amount) {
+        // - счет не должен быть заблокированным
+        if(isBlocked){
+            throw new IllegalStateException("счет не должен быть заблокированным");
+        }
+
+        // - счет должен быть активированным
+        if(this.amount==null){
+            throw new IllegalStateException("счет должен быть активированным");
+        }
+
+        // - нельзя снять больше чем есть на балансе
+        if(amount>this.amount){
+            throw new IllegalStateException("Ваш баланс меньше нуля");
+        }
+
+        this.amount -= amount;
+    }
+
+    // TODO: Перевод средств
+    public void transfer(BankAccount otherAccount, int amount) {
+        // - счет не должен быть заблокированным
+        // - счет должен быть активированным
+        // - нельзя перевести больше чем есть на балансе
+    }
+    public void deposit(int amount) {
+        // при пополнении:
+        // - счет не должен быть заблокированным
+        if (isBlocked) {
+            throw new IllegalStateException("Счёт заблокирован.");
+        }
+
+        // - счет должен быть активированным
+        if (this.amount == null) {
+            throw new IllegalStateException("Счёт не активирован.");
+        }
+
+        // - нельзя пополнять на отрицательную сумму
+        if (amount < 0) {
+            throw new IllegalStateException("Нельзя пополнять на отрицательную сумму");
+        }
+
+        this.amount += amount;
+    }
 
     public void block() {
         this.isBlocked = true;
--- src/TaskJunit/BankAccountTest.java
+++ src/TaskJunit/BankAccountTest.java
@@ -6,6 +6,41 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BankAccountTest {
 
     @Test
+    void shouldNotBeLockedWhenWithdrow(){
+        BankAccount account = new BankAccount("a", "b");
+        account.block();
+        final IllegalStateException ex = assertThrows(IllegalStateException.class, () -> account.withdraw(3));
+        assertEquals("счет не должен быть заблокированным", ex.getMessage());
+
+    }
+    @Test
+    void shouldBeActivetesWhenWithdraw(){
+        BankAccount account = new BankAccount("a", "b");
+        final IllegalStateException ex = assertThrows(IllegalStateException.class, () -> account.withdraw(3));
+        assertEquals("счет должен быть активированным", ex.getMessage());
+
+
+    }
+    @Test
+    void shouldNotBeWithdrawWhenLessAmount(){
+        BankAccount account = new BankAccount("a", "b");
+        account.activate("USD");
+        account.deposit(2);
+        final IllegalStateException ex = assertThrows(IllegalStateException.class, () -> account.withdraw(5));
+
+    }
+
+    @Test
+    void shouldCreatAmountWhenActivatedAndisNotBlocked(){
+        BankAccount account = new BankAccount("a", "b");
+        account.activate("USD");
+        account.deposit(500);
+        account.withdraw(100);
+        assertEquals(400, account.getAmount());
+    }
+
+
+    @Test
     void shouldNotBeBlockedWhenCreated() {
         BankAccount account = new BankAccount("a", "b");
         assertFalse(account.isBlocked());
